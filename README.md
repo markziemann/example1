7178ced8af876727dd683b5bea3d58fd7fe1a7d0
markziemann <mark.ziemann@gmail.com>
2020-04-08 15:25:42 +1000

    added example.R to demonstrate how to download a file and read into R

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # example1
-Tutorial on how to work with GitHub in R
+Tutorial on how to work with GitHub in R.
+
+## Motivation
+This demonstration is designed to get students involved in working with GitHub via Rstudio.
+
+## Contents
+An example R script/Rmd that demonstrates how to download a file and read it into R. 
+
+## Installation
+This script was designed to work with Rstudio version 1.2.1335 and Base R 3.6.3.
---
+++ example.R
@@ -0,0 +1,16 @@
+# download the gene expression dataset from github
+# note that I've downloaded the "raw" file 
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part1_files/mydata.tsv",
+              destfile = "mydata.tsv")
+
+# Read file into R and specify that the 1st line is the header and 
+# first row is the row names, making sure that strings are not recognised
+# as factors
+x <- read.table("mydata.tsv", header = TRUE, row.names=1 ,
+    stringsAsFactors = FALSE)
+
+# Have a look at the first 6 lines of a file
+head(x)
+
+# confirm the structure and type of data in dataframe
+str(x)
